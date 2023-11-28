a2c63a402269274343e47dba81558965e72f7156
Gig2341 <danmata2341@gmail.com>
2023-11-28 03:41:58 +0000

    task 10 done

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const my_array = [];
-  for (const value of my_array) {
+  for (const value of array) {
       my_array.push(appendString + value);
   }
 
-  return array;
+  return my_array;
 }
