051921d6e8d9c3158b591209d490e6c953d8a81c
Gig2341 <danmata2341@gmail.com>
2023-12-06 13:39:30 +0000

    task 4 and 5 fixed

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,12 @@
-export default function updateStudentGradeByCity(arrays, cities, newGrades) {
-  return arrays
-    .filter((city) => city.location === cities);
-    .map((student) => {
-      newGrade.map((studentGrade) => {
-          if (student
+export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  const newlist = getListStudents.filter((student) => student.location === city);
+
+  return newlist.map((student) => {
+    const [newGrade] = newGrades.filter((grade) => grade.studentId === student.id);
+
+    return {
+      ...student,
+      grade: newGrade ? newGrade.grade : 'N/A',
+    };
+  });
+}
